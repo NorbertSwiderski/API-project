44ef58c59db996e2fb4a87f3823fd128cfda4963
Norbert Świderski <norbert.swiderski@gmail.com>
2019-11-26 15:05:30 +0100

    feat: download repositories from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,16 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const list = document.querySelector('.list--js');
+fetch('https://api.github.com/users/NorbertSwiderski/repos')
+  .then(resp => resp.json()).then(resp => {
+    const repos = resp;
+    for (const repo of repos){
+      list.innerHTML+=`<li><a href="${repo.html_url}"> ${repo.name}</a></li>`;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
 
 
